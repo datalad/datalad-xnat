3527c45995708ba6926ba2382e9ec8c64ae2b6f6
Michael Hanke <michael.hanke@gmail.com>
2021-09-27 09:33:23 +0200

    First minimal integration test
    Towards datalad/datalad-xnat#6 and gets us close to 80% test coverage
    already.

---
+++ datalad_xnat/tests/test_xnatcentral.py
@@ -0,0 +1,45 @@
+# emacs: -*- mode: python; py-indent-offset: 4; tab-width: 4; indent-tabs-mode: nil -*-
+# ex: set sts=4 ts=4 sw=4 noet:
+# ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the datalad package for the
+#   copyright and license terms.
+#
+# ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+"""Integration tests for XNAT central
+
+"""
+
+from datalad.api import (
+    Dataset,
+)
+
+from datalad.tests.utils import (
+    assert_in_results,
+    with_tempfile,
+)
+
+
+@with_tempfile
+def test_anonymous_access(path):
+    ds = Dataset(path).create()
+    # minimal demo dataset (pulls .5MB from xnat central)
+    ds.xnat_init(
+        'https://central.xnat.org',
+        project='Sample_DICOM',
+        credential='anonymous')
+    ds.xnat_update(
+        subjects='dcmtest1',
+        # TODO `init` should store this info
+        credential='anonymous',
+    )
+    # we get the project's payload DICOM in the expected location
+    assert_in_results(
+        ds.status(annex='availability'),
+        key='MD5E-s533936--b402d6341f5894c63c991c6361ad14ff.dcm',
+        has_content=True,
+        path=str(ds.pathobj / 'dcmtest1' / 'CENTRAL_E03907' / '2' /
+                 'dcmtest1.MR.Sample_DICOM.2.1.20010108.120022.1azj8tu.dcm'),
+        state='clean',
+        type='file',
+    )
